0205832e0f0f1b6225cd113b908a1adc07067b75
Gil Keidar <keidar.gil@gmail.com>
2025-08-26 18:41:08 -0700

    Basic collector script to get userAgent

--- code/collector.js
+++ code/collector.js
@@ -3,4 +3,32 @@ console.log("hello there");
 addEventListener("load", (event) => {
     console.log("Page has loaded!");
     console.log(event);
+
+    //  Attempt to send the user agent string back to the /json/user-sessions
+
+    let userAgent = navigator.userAgent;
+
+    let data = {
+        id: Math.floor((Math.random() * 100000)),
+        userAgent: userAgent
+    };
+
+    const request = new Request("https://gilkeidar.com/json/user-sessions", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+
+
+    fetch(request)
+        .then((response) => {
+            console.log(`Received response with status code ${response.status}!`);
+            return response.json();
+        })
+        .then((jsonBody) => {
+            console.log("Response data:");
+            console.log(jsonBody);
+        });
 });
